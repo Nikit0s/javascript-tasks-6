1bcf5d2d372c317644e901c27be3449a2c3cad57
Nikita Pestov <grizzlyarchi@gmail.com>
2015-11-10 18:17:02 +0500

    init

--- moment.js
+++ moment.js
@@ -10,6 +10,29 @@ module.exports = function () {
 
         // Выводит дату в переданном формате
         format: function (pattern) {
+            var minutesDate = this.date + parseInt(this.timezone) * 60;
+            var numberToDay = {
+                0: 'ПН',
+                1: 'ВТ',
+                2: 'СР'
+            };
+            var day = parseInt(minutesDate / (60 * 24));
+            minutesDate -= day * 60 * 24;
+            day = numberToDay[day];
+            var hours = parseInt(minutesDate / 60);
+            minutesDate -= hours * 60;
+            hours = hours.toString();
+            if (hours.length === 1) {
+                hours = '0' + hours;
+            }
+            var minutes = minutesDate.toString();
+            if (minutes.length === 1) {
+                minutes = '0' + minutes;
+            }
+            var result = pattern.replace('%DD', day);
+            result = result.replace('%HH', hours);
+            result = result.replace('%MM', minutes);
+            return result;
         },
 
         // Возвращает кол-во времени между текущей датой и переданной `moment`
--- robbery.js
+++ robbery.js
@@ -2,13 +2,95 @@
 
 var moment = require('./moment');
 
+var dateToMinutes = function (rawDate) {
+    var daysToNumber = {
+        'ПН': 0,
+        'ВТ': 60 * 24,
+        'СР': 60 * 48
+    };
+    var re = /[ \:]/;
+    var splittedDate = rawDate.split(re);
+    var minutes = 0;
+    if (splittedDate.length === 3) {
+        var reminder = splittedDate[2].substr(2);
+        splittedDate[2] = splittedDate[2].substr(0, 2);
+        splittedDate.push(reminder);
+        minutes += daysToNumber[splittedDate[0]] + parseInt(splittedDate[1]) * 60 +
+            parseInt(splittedDate[2]) - parseInt(splittedDate[3] * 60);
+        return minutes;
+    }
+    var reminder = splittedDate[1].substr(2);
+    splittedDate.push(reminder);
+    minutes += parseInt(splittedDate[0]) * 60 + parseInt(splittedDate[1]) -
+        parseInt(splittedDate[2] * 60);
+    return minutes;
+};
+
+var checkAvailable = function (thief, gang, minute) {
+    var gaps = gang[thief];
+    for (var i = 0; i < gaps.length; i++) {
+        if ((minute >= gaps[i].from) && (minute <= gaps[i].to)) {
+            return false;
+        }
+    }
+    return true;
+};
+
 // Выбирает подходящий ближайший момент начала ограбления
 module.exports.getAppropriateMoment = function (json, minDuration, workingHours) {
     var appropriateMoment = moment();
+    appropriateMoment.timezone = parseInt(workingHours.from.substr(-2));
 
     // 1. Читаем json
     // 2. Находим подходящий ближайший момент начала ограбления
     // 3. И записываем в appropriateMoment
+    var gang = JSON.parse(json);
+    var friends = Object.keys(gang);
+    friends.forEach(function (person) {
+        var timeGaps = gang[person];
+        timeGaps.forEach(function (gap) {
+            gap.from = dateToMinutes(gap.from);
+            gap.to = dateToMinutes(gap.to);
+        });
+    });
+    var bankOpened = [];
+    var workingStart = dateToMinutes(workingHours.from);
+    var workingEnd = dateToMinutes(workingHours.to);
+    for (var i = 0; i < 3; i++) {
+        var currentDay = {
+            from: i * 60 * 24 + workingStart,
+            to: i * 60 * 24 + workingEnd
+        };
+        bankOpened.push(currentDay);
+    }
+    var thiefs = Object.keys(gang);
+    var momentMinute = -1;
+    for (var i = 0; i < 3; i++) {
+        var currentStart = bankOpened[i].from;
+        var currentEnd = bankOpened[i].to;
+        var clock = 0;
+        for (var j = currentStart; j <= currentEnd; j++) {
+            var allAvailable = true;
+            for (var k = 0; k < thiefs.length; k++) {
+                if (!checkAvailable(thiefs[k], gang, j)) {
+                    clock = 0;
+                    allAvailable = false;
+                    break;
+                }
+            }
+            if (allAvailable) {
+                clock += 1;
+            }
+            if (clock === minDuration) {
+                momentMinute = j - minDuration;
+                break;
+            }
+        }
+        if (momentMinute > -1) {
+            break;
+        }
+    }
+    appropriateMoment.date = momentMinute;
 
     return appropriateMoment;
 };
